6549ba22c0e2bbea221cacbd7efd7245e10678df
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-06-27 23:17:53 +0900

    feat: React-router 설정 #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import ChatRoomListLayout from "@/layout/ChatRoomListLayout";
+import ChatRoomPageLayout from "./layout/ChatRoomPageLayout";
+
 export default function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<ChatRoomListLayout />}></Route>
+      <Route path="/page" element={<ChatRoomPageLayout />}></Route>
+    </Routes>
+  );
 }
