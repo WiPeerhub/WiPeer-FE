39397c20edced13fd5d59d25ff0a0d2f1145e671
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-07-07 17:51:13 +0900

    refactor: IP 변동시 알람을 위한 커스텀 훅 생성 및 적용 #8

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { useEffect, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import ChatRoomListLayout from "@/layout/ChatRoomListLayout";
 import ChatRoomPageLayout from "@/layout/ChatRoomPageLayout";
 import NickNamePage from "@/pages/NickNamePage";
-import useClientIP from "@/hooks/useClientIP";
 
 export default function App() {
-  const clientIP = useClientIP();
-  const prevIP = useRef("");
-
-  useEffect(() => {
-    if (prevIP.current && prevIP.current !== clientIP) {
-      alert(`IP가 변경되었습니다!\n이전: ${prevIP.current}\n현재: ${clientIP}`);
-    }
-
-    prevIP.current = clientIP;
-  }, [clientIP]);
-
   return (
     <Routes>
       <Route path="/" element={<NickNamePage />}></Route>
--- src/layout/ChatRoomPageLayout.jsx
+++ src/layout/ChatRoomPageLayout.jsx
@@ -2,10 +2,14 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ChatRoomPage from "@/pages/ChatRoomPage";
 import CreateRoomPage from "@/pages/CreateRoomPage";
+import useClientIP from "@/hooks/useClientIP";
+import useIPChangeAlert from "@/hooks/useIPChangeAlert";
 import { useRoomCreationStore } from "@/stores/useRoomCreationStore";
 
 export default function ChatRoomPageLayout() {
   const isRoomCreation = useRoomCreationStore((state) => state.isRoomCreation);
+  const clientIP = useClientIP();
+  useIPChangeAlert(clientIP);
 
   return (
     <div className="flex h-screen w-[375px] flex-col justify-between border border-gray-200">
