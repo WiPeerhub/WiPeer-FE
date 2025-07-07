d32f27d03f8a178b00bad044b2c64b53d3a008ae
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-07-07 14:56:11 +0900

    feat: 공인 IP 변경시 알람 기능 구현(useClientIP) #8

--- src/hooks/useClientIP.jsx
+++ src/hooks/useClientIP.jsx
@@ -1,15 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { API } from "@/constants/api";
 
 export default function useClientIP() {
   const [ip, setIP] = useState("");
+  const prevIP = useRef("");
 
   useEffect(() => {
     const fetchIP = async () => {
       try {
         const res = await fetch(API.GET_CLIENT_IP);
         const ipInfo = await res.json();
+        const currentIP = ipInfo.ip;
 
+        if (prevIP.current && prevIP.current !== currentIP) {
+          alert("네트워크 변경이 감지됨");
+        }
+
+        prevIP.current = currentIP;
         setIP(ipInfo.ip);
       } catch (err) {
         console.error("공인 IP 가져오기 실패", err);
@@ -17,6 +24,8 @@ export default function useClientIP() {
     };
 
     fetchIP();
+    const interval = setInterval(fetchIP, 10000);
+    return () => clearInterval(interval);
   }, []);
 
   return ip;
