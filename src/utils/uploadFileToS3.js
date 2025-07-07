efa1ac39e0ae117bf186bbb5f8e04a8342ffed11
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-07-07 11:21:06 +0900

    fix: File 객체 미전달로 인한 S3 이미지 액박 이슈 조치 #7

--- src/utils/uploadFileToS3.js
+++ src/utils/uploadFileToS3.js
@@ -2,6 +2,9 @@ import { API } from "@/constants/api";
 
 export async function uploadFileToS3(file) {
   const res = await fetch(API.uploadFile(file.type, file.name));
+  console.log("File:", file);
+  console.log(file.name, file.type, file.size);
+  console.log(file.file instanceof File);
 
   const { uploadUrl, downloadUrl, fileUrl } = await res.json();
 
@@ -10,7 +13,7 @@ export async function uploadFileToS3(file) {
     headers: {
       "Content-Type": file.type,
     },
-    body: file,
+    body: file.file,
   });
 
   if (!uploadRes.ok) throw new Error("S3 업로드 실패");
