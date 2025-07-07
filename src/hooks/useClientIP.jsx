3f30531dfe9d1aeb6a45904a7b7576d7d3f73a33
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-07-07 17:17:00 +0900

    feat: 네트워크 on/off, 품질 변경 기반의 IP 요청 기능 구현 #8

--- src/hooks/useClientIP.jsx
+++ src/hooks/useClientIP.jsx
@@ -1,31 +1,42 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { API } from "@/constants/api";
 
 export default function useClientIP() {
   const [ip, setIP] = useState("");
   const prevIP = useRef("");
 
-  useEffect(() => {
-    const fetchIP = async () => {
-      try {
-        const res = await fetch(API.GET_CLIENT_IP);
-        const ipInfo = await res.json();
-        const currentIP = ipInfo.ip;
+  const fetchIP = async () => {
+    try {
+      const res = await fetch(API.GET_CLIENT_IP);
+      const ipInfo = await res.json();
+      const currentIP = ipInfo.ip;
 
-        if (prevIP.current && prevIP.current !== currentIP) {
-          alert("네트워크 변경이 감지됨");
-        }
-
+      if (currentIP !== prevIP.current) {
+        setIP(currentIP);
         prevIP.current = currentIP;
-        setIP(ipInfo.ip);
-      } catch (err) {
-        console.error("공인 IP 가져오기 실패", err);
+      }
+    } catch (err) {
+      console.error("공인 IP 가져오기 실패", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchIP();
+
+    window.addEventListener("online", fetchIP);
+
+    const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+
+    if (connection) {
+      connection.addEventListener("change", fetchIP);
+    }
+
+    return () => {
+      window.removeEventListener("online", fetchIP);
+      if (connection) {
+        connection.removeEventListener("change", fetchIP);
       }
     };
-
-    fetchIP();
-    const interval = setInterval(fetchIP, 10000);
-    return () => clearInterval(interval);
   }, []);
 
   return ip;
