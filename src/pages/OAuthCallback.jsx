dac8b5bf10b6dd0370a334ada9ba2334da208565
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-07-14 13:52:29 +0900

    fix: user token 저장 방식 변경(localStorage -> HttpOnly Cookie) #14

--- src/pages/OAuthCallback.jsx
+++ src/pages/OAuthCallback.jsx
@@ -1,45 +1,33 @@
 import { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { useAuthStore } from "@/stores/useAuthStore";
+import { useNavigate } from "react-router-dom";
 import { useUserStore } from "@/stores/useUserStore";
 
 export default function OAuthCallback() {
-  const setToken = useAuthStore((state) => state.setToken);
   const setUserInfo = useUserStore((state) => state.setUserInfo);
   const navigate = useNavigate();
-  const location = useLocation();
 
   useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const token = params.get("token");
+    const fetchUser = async () => {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/user/me`, {
+        method: "GET",
+        credentials: "include",
+      });
 
-    if (token) {
-      setToken(token);
-      const fetchUser = async () => {
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/user/me`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+      if (!res.ok) {
+        console.error("요청에 실패하였습니다.");
+        navigate("/login", { replace: true });
+        return;
+      }
 
-        if (!res.ok) {
-          console.error("요청에 실패하였습니다.");
-          return;
-        }
-
-        const userInfo = await res.json();
-        setUserInfo(userInfo);
-        localStorage.setItem("ownerId", userInfo.id);
-        console.log(userInfo);
-      };
-
-      fetchUser();
-
+      const userInfo = await res.json();
+      setUserInfo(userInfo);
+      localStorage.setItem("ownerId", userInfo.id);
+      console.log(userInfo);
       navigate("/", { replace: true });
-    } else {
-      navigate("/login", { replace: true });
-    }
-  }, [location, navigate]);
+    };
+
+    fetchUser();
+  }, [navigate]);
 
   return <div>로그인 중...</div>;
 }
