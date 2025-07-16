7acfccb8d781c8515ca7058198de10d48b23dae1
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-07-16 17:22:47 +0900

    chore: 내 활동 -> 내 활동 방 기획 변경으로 인한 Layout 수정 #13

--- src/layout/ChatRoomListLayout.jsx
+++ src/layout/ChatRoomListLayout.jsx
@@ -1,40 +1,20 @@
-import { useState } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ChatRoomListPage from "@/pages/ChatRoomListPage";
 import CreateRoomPage from "@/pages/CreateRoomPage";
 import { useRoomCreationStore } from "@/stores/useRoomCreationStore";
-import ChatMyRoomListPage from "@/pages/ChatMyRoomListPage";
 
 export default function ChatRoomListLayout() {
-  const [isToggled, setIsToggled] = useState(false);
   const isRoomCreation = useRoomCreationStore((state) => state.isRoomCreation);
 
   return (
     <div className="h-screen-dvh flex w-[375px] flex-col justify-between border border-gray-200">
       <Header />
-      <div className="flex items-center justify-center gap-10 border-b border-gray-200 p-4">
-        <button
-          onClick={() => setIsToggled(false)}
-          className={`relative text-lg font-semibold transition-all duration-200 ${
-            !isToggled ? "text-blue-600" : "text-gray-900"
-          }`}
-        >
-          채팅방 목록
-          {!isToggled && <span className="absolute right-0 bottom-0 left-0 h-0.5 bg-blue-500" />}
-        </button>
-        <button
-          onClick={() => setIsToggled(true)}
-          className={`relative text-lg font-semibold transition-all duration-200 ${
-            isToggled ? "text-blue-600" : "text-gray-900"
-          }`}
-        >
-          나의 방{isToggled && <span className="absolute right-0 bottom-0 left-0 h-0.5 bg-blue-500" />}
-        </button>
+      <div className="flex items-center justify-between border-b border-gray-200 p-4">
+        <h2 className="text-lg font-semibold text-gray-900">채팅방 목록</h2>
       </div>
-
       <main className="hide-scrollbar flex-1 overflow-y-auto">
-        {!isToggled ? <ChatRoomListPage /> : <ChatMyRoomListPage />}
+        <ChatRoomListPage />
       </main>
       <Footer />
       {isRoomCreation && <CreateRoomPage />}
