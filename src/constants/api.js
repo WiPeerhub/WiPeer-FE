7bd9cc89c34b5b95253ab169a914d49df6713c42
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-07-16 17:19:29 +0900

    chore: roomAPI 전체 수정으로 인한 기존 API 요청 함수 삭제 #16

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,13 +2,10 @@ export const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 export const API = {
   GET_CLIENT_IP: `${BASE_URL}/ip`,
-  POST_ROOM: `${BASE_URL}/room`,
   DELETE_S3_File: `${BASE_URL}/upload/delete-files`,
   updateMessage: (roomId, messageId) => `${BASE_URL}/${roomId}/message/${messageId}`,
   updateReactions: (roomId, messageId) => `${BASE_URL}/${roomId}/message/${messageId}/reactions`,
   deleteMessage: (roomId, messageId) => `${BASE_URL}/${roomId}/message/${messageId}`,
   getLastMessage: (roomId) => `${BASE_URL}/last-message/${roomId}`,
   uploadFile: (fileType, fileName) => `${BASE_URL}/upload/upload-url?fileType=${fileType}&fileName=${fileName}`,
-  deleteRoom: (roomId) => `${BASE_URL}/room/${roomId}`,
-  getRoomsByIP: (ip) => `${BASE_URL}/room?ip=${ip}`,
 };
